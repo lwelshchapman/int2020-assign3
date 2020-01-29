d39bc7d037da66bc1d5000a4c27a831df5c01e8a
lwelshchapman <lwelsh@chapman.edu>
2020-01-29 21:41:51 +0000

    Changed main.cpp to use different constructors

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@
 
 int main(int argv, char** argc) {
 	
-	//cout << "sup fella" << endl;
+	// INSTANTIATE
+	Pet *p1 = new Pet("Sparky", 3, "dog", 80.5);	// Constructed with values
 	
-	Pet *p1 = new Pet("Sparky", 3, "dog", 80.5);
-	Pet *p2 = new Pet("Oscar", 5, "cat", 8.12);
+	Pet *p2 = new Pet();	// Constructed empty, values set with mutators
+	p2->setName("Oscar");
+	p2->setAge(5);
+	p2->setType("cat");
+	p2->setWeight(8.12);	
 	
+	
+	// OUTPUT VALUES
 	cout << p1->toString() << endl;
 	cout << p2->toString() << endl;
 	
